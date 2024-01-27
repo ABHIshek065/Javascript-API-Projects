126be2dc5d12379486f88fb9ec1f841cdc5f5f99
Abhishek Patel <98528020+Abhi8755@users.noreply.github.com>
2024-01-27 21:41:39 +0530

    update

--- 04Memes_app/app.js
+++ 04Memes_app/app.js
@@ -1,13 +1,9 @@
 
-
-
-
 let meme = document.getElementById("meme");
 let title = document.getElementById("title");
 let getMemeBtn = document.getElementById("get-meme-btn");
 //API URL
 let url = "https://meme-api.com/gimme/";
-//Array of subreddits of your choice
 let subreddits = ["catmemes", "wholesomemes", "dogmemes", "me_irl"];
 console.log(url);
 //Function To Get Random Meme
